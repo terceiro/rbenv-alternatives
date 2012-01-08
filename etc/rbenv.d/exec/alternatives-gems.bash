e1f5686e2daa5fb7c5461eaf551b0f1c935efabd
Antonio Terceiro <terceiro@softwarelivre.org>
2012-01-08 17:27:52 -0200

    Hooks to deal with rebenv-version local GEM_HOME's

---
+++ etc/rbenv.d/exec/alternatives-gems.bash
@@ -0,0 +1,10 @@
+__current_version=$(rbenv-version-name)
+case "$__current_version" in
+  *-debian)
+    export GEM_HOME="${RBENV_ROOT}/versions/${__current_version}/gems"
+    mkdir -p "${GEM_HOME}"
+    ;;
+  *)
+    true
+    ;;
+esac
---
+++ etc/rbenv.d/rehash/alternatives-gems.bash
@@ -0,0 +1,13 @@
+for version in "${RBENV_ROOT}/versions/"*-debian; do
+  version_name=$(basename "$version")
+  gem_bindir="${RBENV_ROOT}/versions/${version_name}/gems/bin"
+  version_bindir="${RBENV_ROOT}/versions/${version_name}/bin"
+  echo "$gem_bindir -> $version_bindir"
+  if [[ -d "$gem_bindir" ]]; then
+    for program in "$gem_bindir"/*; do
+      program_name="$(basename $program)"
+      ln -sf "$(readlink -f "$program")" "$version_bindir/${program_name}"
+      register_shim "$program_name"
+    done
+  fi
+done
