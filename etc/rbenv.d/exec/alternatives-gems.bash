78723417f03f9b8455c2a010a05607634f2d6019
Antonio Terceiro <terceiro@softwarelivre.org>
2012-01-10 12:05:05 -0200

    No need to create the dir pointed to by GEM_HOME

--- etc/rbenv.d/exec/alternatives-gems.bash
+++ etc/rbenv.d/exec/alternatives-gems.bash
@@ -2,7 +2,6 @@ __current_version=$(rbenv-version-name)
 case "$__current_version" in
   *-debian)
     export GEM_HOME="${RBENV_ROOT}/versions/${__current_version}/gems"
-    mkdir -p "${GEM_HOME}"
     ;;
   *)
     true
