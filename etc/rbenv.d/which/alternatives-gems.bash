a8b4a540d040e7feaec35f145edb43102da76367
Jake Teton-Landis <jake.tl@airbnb.com>
2017-04-04 12:12:53 -0700

    $version/is-debian-alternative marker file
    Instead of relying on the rbenv version name to end in "-debian", we now
    check for the existence of a is-debian-alternative marker file to
    determine if a given rbenv version is a debian alternative.
    
    This has the advantage of allowing a user to symlink a
    .rbenv/versions/2.1.8-debian alternative to .rbenv/versions/2.1.8, and
    then use existing .ruby-version files that specify 2.1.8.

--- etc/rbenv.d/which/alternatives-gems.bash
+++ etc/rbenv.d/which/alternatives-gems.bash
@@ -1,8 +1,6 @@
-case "$RBENV_VERSION" in
-  *-debian)
-    command_path="${RBENV_ROOT}/versions/${RBENV_VERSION}/gems/bin/${RBENV_COMMAND}"
-    if ! [ -x "$RBENV_COMMAND_PATH" ] && [ -x "$command_path" ]; then
-      RBENV_COMMAND_PATH="$command_path"
-    fi
-    ;;
-esac
+if [ -e "${RBENV_ROOT}/versions/${RBENV_VERSION}/is-debian-alternative" ]; then
+  command_path="${RBENV_ROOT}/versions/${RBENV_VERSION}/gems/bin/${RBENV_COMMAND}"
+  if ! [ -x "$RBENV_COMMAND_PATH" ] && [ -x "$command_path" ]; then
+    RBENV_COMMAND_PATH="$command_path"
+  fi
+fi
